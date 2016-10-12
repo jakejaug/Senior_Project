828568f4a0f4212dcac73a89aca599125dfd41dc
Jake <Jake@JAKE>
2016-10-12 16:05:09 -0400

    Try on pi

--- Circle Tracking/HSV Tracking/Circles.cpp
+++ Circle Tracking/HSV Tracking/Circles.cpp
@@ -12,25 +12,31 @@
 using namespace cv;
 using namespace std;
 
-vector<double> filter(vector<double> v) {	//Filters extra circles out
-	double sum = std::accumulate(v.begin(), v.end(), 0.0);	//Calculate mean
-	double mean = sum / v.size();
-	double sq_sum = std::inner_product(v.begin(), v.end(), v.begin(), 0.0);
-	double stdev = std::sqrt(sq_sum / v.size() - mean * mean);	//Calculate standard deviation
-	double hi = mean + stdev;
-	double low = mean - stdev;
+vector<double> filter(vector<double> u, vector<double> v) {	//Filters extra circles out
+	double devscale = 1.5;
+	double sumv = std::accumulate(v.begin(), v.end(), 0.0);	//Calculate mean
+	double meanv = sumv / v.size();
+	double sq_sumv = std::inner_product(v.begin(), v.end(), v.begin(), 0.0);
+	double stdevv = std::sqrt(sq_sumv / v.size() - meanv * meanv);	//Calculate standard deviation
+	double hiv = meanv + devscale*stdevv;
+	double lowv = meanv - devscale*stdevv;
+	double sumu = std::accumulate(u.begin(), u.end(), 0.0);	//Calculate mean
+	double meanu = sumu / u.size();
+	double sq_sumu = std::inner_product(u.begin(), u.end(), u.begin(), 0.0);
+	double stdevu = std::sqrt(sq_sumu / u.size() - meanu * meanu);	//Calculate standard deviation
+	double hiu = meanu + devscale*stdevu;
+	double lowu = meanu - devscale*stdevu;
 	vector<double> fil;
-	for (int i = 0; i < v.size(); i++) {
-		if ((v[i] > low) && (v[i] < hi))	//Filter out values
+	//cout << u.size() << " + " << v.size() << endl;
+	for (int i = 0; i < (v.size()); i++) {
+		if (((u[i] > lowu) && (u[i] < hiu)) && ((v[i] > lowv) && (v[i] < hiv))) {
+			fil.push_back(u[i]);
 			fil.push_back(v[i]);
+		}
 	}
 	return fil;	//return new vector
 }
-double stddev(double mean, vector<double> v){		//Calculates Standard Deviation of a vector
-	double sq_sum = std::inner_product(v.begin(), v.end(), v.begin(), 0.0);
-	double stdev = std::sqrt(sq_sum / v.size() - mean * mean);
-	return stdev;
-}
+
 
 int main(int argc, char** argv)
 {
@@ -53,16 +59,18 @@ int main(int argc, char** argv)
 		vector<double> ycoord;  //vector used to store y-coordinates of circles
 		for (size_t i = 0; i < circles.size(); i++) {
 			Vec3i c = circles[i];
-			circle(img, Point(c[0], c[1]), c[2], Scalar(0, 0, 255), 3, LINE_AA);	//Draws detected circles in image
+			//circle(img, Point(c[0], c[1]), c[2], Scalar(0, 0, 255), 3, LINE_AA);	//Draws detected circles in image
 			//circle(img, Point(c[0], c[1]), 2, Scalar(255, 0, 0), 3, LINE_AA);
 			xcoord.push_back(c[0]); //store x values in xcoord
 			ycoord.push_back(c[1]); //store y values in ycoord
 		}
-		vector<double> x = filter(xcoord);
-		vector<double> y = filter(ycoord);
-		if ((x.size() == 6) || (y.size() == 6))
-			for(int i = 0; i<7; i++)
-				circle(img, Point(x[i], y[i]), 2, Scalar(255, 0, 0), 3, LINE_AA);
+		vector<double> filr = filter(xcoord, ycoord);
+		cout << filr.size() << endl;
+		if (filr.size()==12){
+			//for (int i = 0; i < 12; i + 2)
+				//circle(img, Point(filr[i], filr[i+1]), 2, Scalar(255, 0, 0), 3, LINE_AA);
+				cout << "target" << endl;
+		}
 
 		imshow("detected circles", img); //Show detected circles
 		if(waitKey(100)=='q')	//hold 'q' to quit program
